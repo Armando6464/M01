a5541b3fadf876540fe75e20ce7b42312a8ea2a9
Armando6464 <armando95_95@hotmail.com>
2017-01-13 01:34:30 +0100

    Añadida dependencia con log4j e implementados en App.java

--- src/main/java/es/egc/app/App.java
+++ src/main/java/es/egc/app/App.java
@@ -1,4 +1,5 @@
 package es.egc.app;
+import org.apache.log4j.*;
 
 /**
  * Hello world!
@@ -6,8 +7,11 @@ package es.egc.app;
  */
 public class App 
 {
+	static Logger log=Logger.getLogger(App.class);
+	
     public static void main( String[] args )
     {
+		log.info("Returning 1");
         System.out.println( "Hello World!" );
     }
 	
